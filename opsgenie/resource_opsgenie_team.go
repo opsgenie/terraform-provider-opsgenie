434b75866a761ea9a8f3046366ee60af41b1eec8
Arnold Bechtoldt <arnold.bechtoldt@inovex.de>
2020-03-10 15:36:03 +0100

    add optional setting to stop maintaing team members, refs #65
    Signed-off-by: Arnold Bechtoldt <arnold.bechtoldt@inovex.de>

--- opsgenie/resource_opsgenie_team.go
+++ opsgenie/resource_opsgenie_team.go
@@ -30,6 +30,11 @@ func resourceOpsGenieTeam() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
+			"manage_members": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
 			"member": {
 				Type:     schema.TypeList,
 				Optional: true,
@@ -63,10 +68,13 @@ func resourceOpsGenieTeamCreate(d *schema.ResourceData, meta interface{}) error 
 	createRequest := &team.CreateTeamRequest{
 		Name:        name,
 		Description: description,
-		Members:     expandOpsGenieTeamMembers(d),
 	}
 
-	log.Printf("[INFO] Creating OpsGenie team '%s'", name)
+	if len(d.Get("member").([]interface{})) > 0 && d.Get("manage_members").(bool) {
+		createRequest.Members = expandOpsGenieTeamMembers(d)
+	}
+
+	log.Printf("[INFO] Creating OpsGenie team %q", name)
 
 	_, err = client.Create(context.Background(), createRequest)
 	if err != nil {
@@ -99,6 +107,8 @@ func resourceOpsGenieTeamRead(d *schema.ResourceData, meta interface{}) error {
 		IdentifierValue: d.Id(),
 	}
 
+	log.Printf("[INFO] Retrieving state of OpsGenie team '%s'", d.Get("name"))
+
 	getResponse, err := client.Get(context.Background(), getRequest)
 	if err != nil {
 		return err
@@ -106,7 +116,10 @@ func resourceOpsGenieTeamRead(d *schema.ResourceData, meta interface{}) error {
 
 	d.Set("name", getResponse.Name)
 	d.Set("description", getResponse.Description)
-	d.Set("member", flattenOpsGenieTeamMembers(getResponse.Members))
+
+	if d.Get("manage_members").(bool) {
+		d.Set("member", flattenOpsGenieTeamMembers(getResponse.Members))
+	}
 
 	return nil
 }
@@ -123,7 +136,10 @@ func resourceOpsGenieTeamUpdate(d *schema.ResourceData, meta interface{}) error 
 		Id:          d.Id(),
 		Name:        name,
 		Description: description,
-		Members:     expandOpsGenieTeamMembers(d),
+	}
+
+	if len(d.Get("member").([]interface{})) > 0 && d.Get("manage_members").(bool) {
+		updateRequest.Members = expandOpsGenieTeamMembers(d)
 	}
 
 	log.Printf("[INFO] Updating OpsGenie team '%s'", name)
--- opsgenie/resource_opsgenie_team_test.go
+++ opsgenie/resource_opsgenie_team_test.go
@@ -74,6 +74,108 @@ func TestAccOpsGenieTeam_basic(t *testing.T) {
 	})
 }
 
+func TestAccOpsGenieTeam_basicNoMember(t *testing.T) {
+	randomTeam := acctest.RandString(6)
+	randomUser := acctest.RandString(6)
+	config := testAccOpsGenieTeam_basicNoMember(randomUser, randomTeam)
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		CheckDestroy: testCheckOpsGenieTeamDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testCheckOpsGenieTeamExists("opsgenie_team.test"),
+					testEmptyMemberList("opsgenie_team.test"),
+					addTeamMembers("opsgenie_team.test", "opsgenie_user.test"),
+				),
+			},
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					testEmptyMemberList("opsgenie_team.test"),
+					removeAllTeamMembers("opsgenie_team.test"),
+				),
+			},
+		},
+	})
+}
+
+func testEmptyMemberList(teamName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		teamResource, ok := s.RootModule().Resources[teamName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", teamName)
+		}
+
+		if len(teamResource.Primary.Attributes["member"]) > 0 {
+			return fmt.Errorf("member list in state of resource %q is unexpectely not empty", teamName)
+		}
+
+		return nil
+	}
+}
+
+func addTeamMembers(teamName string, userName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		teamResource, ok := s.RootModule().Resources[teamName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", teamName)
+		}
+
+		userResource, ok := s.RootModule().Resources[userName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", userName)
+		}
+
+		updateRequest := team.UpdateTeamRequest{
+			Id:          teamResource.Primary.Attributes["id"],
+			Name:        teamResource.Primary.Attributes["name"],
+			Description: teamResource.Primary.Attributes["description"],
+			Members: []team.Member{
+				{
+					User: team.User{ID: userResource.Primary.ID},
+				},
+			},
+		}
+
+		return updateTeamMembers(s, updateRequest)
+	}
+}
+
+func removeAllTeamMembers(teamName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		teamResource, ok := s.RootModule().Resources[teamName]
+		if !ok {
+			return fmt.Errorf("Not found: %s", teamName)
+		}
+
+		updateRequest := team.UpdateTeamRequest{
+			Id:          teamResource.Primary.Attributes["id"],
+			Name:        teamResource.Primary.Attributes["name"],
+			Description: teamResource.Primary.Attributes["description"],
+			Members:     []team.Member{},
+		}
+
+		return updateTeamMembers(s, updateRequest)
+	}
+}
+
+func updateTeamMembers(s *terraform.State, updateRequest team.UpdateTeamRequest) error {
+	client, err := team.NewClient(testAccProvider.Meta().(*OpsgenieClient).client.Config)
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Update(context.Background(), &updateRequest)
+	if err != nil {
+		return fmt.Errorf("error updating team %q with members %q", updateRequest.Name, updateRequest.Members)
+	}
+
+	return nil
+}
+
 func TestAccOpsGenieTeam_complete(t *testing.T) {
 	randomTeam := acctest.RandString(6)
 	randomUser := acctest.RandString(6)
@@ -154,6 +256,22 @@ resource "opsgenie_team" "test" {
   description = "This team deals with all the things"
 }
 `, rString)
+}
+
+func testAccOpsGenieTeam_basicNoMember(randomUser, rString string) string {
+	return fmt.Sprintf(`
+resource "opsgenie_team" "test" {
+  name           = "genieteam-%s"
+  description    = "This team deals with all the things"
+  manage_members = false
+  depends_on     = [opsgenie_user.test] # Just a hack for the test to destroy resources in the right order
+}
+resource "opsgenie_user" "test" {
+  username   = "genietest-%s@opsgenie.com"
+  full_name  = "Acceptance Test User"
+  role       = "User"
+}
+`, randomUser, rString)
 }
 
 func testAccOpsGenieTeam_complete(randomUser, randomTeam string) string {
